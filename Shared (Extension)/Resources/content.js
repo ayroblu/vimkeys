03266a067a6d61c0cdb5d283a7ca1731e4a8439a
Ben Lu <ayroblu@gmail.com>
2023-03-28 03:39:01 +0100

    trying out stuff

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -7,22 +7,31 @@ browser.runtime.onMessage.addListener((request, _sender, _sendResponse) => {
 });
 let isActive = false;
 addEventListener("keydown", (event) => {
-    //    if( e.target.nodeName == "INPUT" || e.target.nodeName == "TEXTAREA" ) return;
-    //    if( e.target.isContentEditable ) return;
     if (!isActive) {
-        if (event.key === '"') {
-            event.preventDefault();
-            isActive = true;
+        if (event.target instanceof HTMLElement &&
+            (event.target.nodeName == "INPUT" ||
+                event.target.nodeName == "TEXTAREA" ||
+                event.target.isContentEditable)) {
+            if (event.key === '"' && event.ctrlKey) {
+                event.preventDefault();
+                isActive = true;
+            }
+        }
+        else {
+            if (event.key === '"') {
+                event.preventDefault();
+                isActive = true;
+            }
         }
         return;
     }
     if (event.key === "j") {
         event.preventDefault();
-        scrollBy(0, -50);
+        scrollBy(0, 50);
     }
     else if (event.key === "k") {
         event.preventDefault();
-        scrollBy(0, 50);
+        scrollBy(0, -50);
     }
     else if (event.key === "d") {
         event.preventDefault();
