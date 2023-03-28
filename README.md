d19847ffdee705ccebf6233fdf7d70ec0156fe9d
Ben Lu <ayroblu@gmail.com>
2023-03-28 03:02:54 +0100

    add prettier and typescript

---
+++ README.md
@@ -0,0 +1,13 @@
+vimkeys
+=======
+
+This is a safari extension in the spirit of vimium-c
+
+TODO
+----
+
+1. Create a build system for TypeScript -> content.js
+2. Build configuration panel
+3. Build core features
+4. Build system so that I can update it continuously
+
---
+++ src/content.js
@@ -0,0 +1,39 @@
+browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+  console.log("Received response: ", response);
+});
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  console.log("Received request: ", request);
+});
+let isActive = false;
+addEventListener(
+  "keydown",
+  (event) => {
+    //    if( e.target.nodeName == "INPUT" || e.target.nodeName == "TEXTAREA" ) return;
+    //    if( e.target.isContentEditable ) return;
+    if (!isActive) {
+      if (event.key === '"') {
+        event.preventDefault();
+        isActive = true;
+      }
+      return;
+    }
+    if (event.key === "j") {
+      event.preventDefault();
+      scrollBy(0, 50);
+    } else if (event.key === "k") {
+      event.preventDefault();
+      scrollBy(0, -50);
+    } else if (event.key === "d") {
+      event.preventDefault();
+      scrollBy(0, window.innerHeight / 2);
+    } else if (event.key === "u") {
+      event.preventDefault();
+      scrollBy(0, -window.innerHeight / 2);
+    } else if (event.key === "'") {
+      event.preventDefault();
+      isActive = false;
+    }
+  },
+  true
+);
